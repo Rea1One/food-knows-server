c8a4c2a7670158bb37622d21a667113a6165c8f5
Wang Chucheng <wangchch99@gmail.com>
2019-11-20 01:25:40 +0800

    Initial commit

---
+++ src/main/java/com/foodknows/server/HelloController.java
@@ -0,0 +1,14 @@
+package com.foodknows.server;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "津ICP备19004351号";
+    }
+
+}
